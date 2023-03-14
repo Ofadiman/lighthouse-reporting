b72c80a64efbca0c6595ef582210f0d55a57b5f8
ofadiman <ofadiman@gmail.com>
2023-03-14 21:39:01 +0100

    feature: configure lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,11 +1,19 @@
 module.exports = {
   ci: {
     assert: {
-      preset: 'lighthouse:recommended',
+      preset: 'lighthouse:no-pwa',
     },
     collect: {
-      url: ['http://localhost:3000/'],
+      settings: {},
+      numberOfRuns: 5,
+      url: [
+        'http://localhost:3000/',
+        'http://localhost:3000/pricing',
+        'http://localhost:3000/support',
+      ],
       startServerCommand: 'pnpm start',
+      startServerReadyPattern: 'ready - started server on 0.0.0.0:3000, url: http://localhost:3000',
+      startServerReadyTimeout: 500,
     },
     upload: {
       target: 'temporary-public-storage',
